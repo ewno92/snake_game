2391a959089551f3bf6accfc18e7f0056bfaf9a4
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-02-10 12:59:22 -0800

    complete

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ while game_on:
         scoreboard.increase_score()
 
     # detect collision with wall
-    if (snake.head.xcor() > 280) or (snake.head.xcor() < -280) or (snake.head.ycor() > 280) or (snake.head.ycor() < -280):
+    if (snake.head.xcor() > 290) or (snake.head.xcor() < -290) or (snake.head.ycor() > 290) or (snake.head.ycor() < -290):
         game_on = False
         scoreboard.game_over()
 
